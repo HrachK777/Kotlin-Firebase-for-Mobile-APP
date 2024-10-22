b609595374ee4dc1f3269ac0393789fcc05229fe
Software Developer <sw2024@cesaeprt.wan>
2024-10-22 16:09:21 +0100

    Update a item_card_view para fazer display de varias cards no mesmo ecra, implemento adapter de addReviewCard e recycler view para users

--- app/src/main/java/ly/roast/roastly/data/model/User.kt
+++ app/src/main/java/ly/roast/roastly/data/model/User.kt
@@ -1,8 +1,39 @@
 package ly.roast.roastly.data.model
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ly.roast.roastly.R
+
 data class User(
-    val name: String,
-    val surname: String,
-    val email: String,
-    val job: String
+    val name: String = "",
+    val surname: String = "",
+    val email: String = "",
+    val job: String = "",
 )
+
+class UserAdapter(private val userList: List<User>) :
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.profile_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_card_view, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = userList[position]
+        holder.nameTextView.text = user.name
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
--- app/src/main/java/ly/roast/roastly/ui/review/AddFragment.kt
+++ app/src/main/java/ly/roast/roastly/ui/review/AddFragment.kt
@@ -1,18 +1,60 @@
 package ly.roast.roastly.ui.review
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import ly.roast.roastly.R
+import ly.roast.roastly.data.model.User
+import ly.roast.roastly.data.model.UserAdapter
 
 class AddFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add, container, false)
+    private lateinit var userAdapter: UserAdapter
+    private val userList = mutableListOf<User>()
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        userAdapter = UserAdapter(userList)
+        recyclerView.adapter = userAdapter
+
+        fetchUsersFromFirestore()
+
+        return view
+    }
+
+    private fun fetchUsersFromFirestore() {
+        val firestore = FirebaseFirestore.getInstance()
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
+
+        firestore.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                userList.clear()
+
+                for (document in result) {
+                    if (document.id != currentUserId) {
+                        val user = document.toObject(User::class.java)
+                        userList.add(user)
+                    }
+                }
+
+                userAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FirestoreError", "Error getting users: ", exception)
+            }
     }
 }
