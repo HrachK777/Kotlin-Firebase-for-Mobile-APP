082746ae837f87ca196a24bb6ba909a0a9a2e319
Juan <juanpereiracosta@gmail.com>
2024-10-28 15:52:29 +0000

    Atualizações no fragment_profile e ProfileFragment

--- app/src/main/java/ly/roast/roastly/ProfileFragment.kt
+++ app/src/main/java/ly/roast/roastly/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.FragmentManager
 
 class ProfileFragment : Fragment() {
 
@@ -17,6 +19,20 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Obtém a referência para o icon_menu
+        val iconMenu = view.findViewById<ImageView>(R.id.icon_menu)
+
+        // Configura o listener de clique
+        iconMenu.setOnClickListener {
+            // Adiciona o FragmentMenu abaixo do icon_menu
+            val fragmentMenu = MenuFragment()
+            childFragmentManager.beginTransaction()
+                .replace(R.id.fragment_menu_container, fragmentMenu) // Certifique-se de que este ID corresponde ao seu layout
+                .commit()
+        }
+
+        return view
     }
 }
