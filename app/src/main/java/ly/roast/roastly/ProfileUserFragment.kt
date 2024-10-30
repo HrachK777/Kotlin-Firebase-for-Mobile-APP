caa95ca6015b98125c9aa60f526030c55748efcb
Juan Costa <juanpereiracosta@gmail.com>
2024-10-30 16:40:08 +0000

    Updates

--- app/src/main/java/ly/roast/roastly/ProfileUserFragment.kt
+++ app/src/main/java/ly/roast/roastly/ProfileUserFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 
 class ProfileUserFragment : Fragment() {
 
@@ -18,24 +17,6 @@ class ProfileUserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the XML layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_profile_user, container, false)
-
-        // Find the ImageView with ID 'icon_menu' in the layout
-        val iconMenu = view.findViewById<ImageView>(R.id.icon_menu)
-
-        // Set a click listener on 'iconMenu'
-        iconMenu.setOnClickListener {
-
-            // Create a new instance of MenuFragment to display
-            val fragmentMenu = MenuFragment()
-
-            // Replace the FrameLayout with ID 'fragment_menu_container' with the MenuFragment instance
-            childFragmentManager.beginTransaction()
-                .replace(R.id.fragment_menu_container, fragmentMenu)
-                .commit()
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_profile_user, container, false)
     }
 }
-
--- app/src/main/java/ly/roast/roastly/ProfilesActivity.kt
+++ app/src/main/java/ly/roast/roastly/ProfilesActivity.kt
@@ -1,20 +1,26 @@
-package ly.roast.roastly
-
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
-class ProfilesActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profiles)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.fragment.app.FragmentTransaction
+//import ly.roast.roastly.ProfileUserFragment
+//import ly.roast.roastly.R
+//import ly.roast.roastly.ui.profile.ProfileFragment
+//
+//class ProfilesActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_profiles)
+//
+//        // Inicia a transação para adicionar o Fragment ao FrameLayout
+//        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+//
+//        // Cria uma nova instância do Fragment que deseja exibir
+//        val profileFragment = ProfileUserFragment() // Supondo que o Fragment se chame ProfileFragment
+//
+//        // Insere o Fragment no FrameLayout com o id "fragment_menu_container_user"
+//        fragmentTransaction.replace(R.id.fragment_menu_container_user, profileFragment)
+//
+//        // Confirma a transação
+//        fragmentTransaction.commit()
+//    }
+//}
