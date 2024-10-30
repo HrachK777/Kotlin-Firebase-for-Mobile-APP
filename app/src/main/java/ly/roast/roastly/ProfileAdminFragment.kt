cbc4615d68166f1e47c5ed35e1c52f70ff101937
Juan <juanpereiracosta@gmail.com>
2024-10-30 00:55:04 +0000

    Updates

--- app/src/main/java/ly/roast/roastly/ProfileAdminFragment.kt
+++ app/src/main/java/ly/roast/roastly/ProfileAdminFragment.kt
@@ -5,55 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import ly.roast.roastly.databinding.FragmentProfileAdminBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileAdminFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileAdminFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileAdminFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileAdminBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_admin, container, false)
+    ): View {
+        _binding = FragmentProfileAdminBinding.inflate(inflater, container, false)
+
+        // Configura o OnClickListener para o icon_menu usando View Binding
+        binding.iconMenu.setOnClickListener(this)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileAdminFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileAdminFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.icon_menu -> {
+                // Exibe o MenuFragment no container especificado
+                parentFragmentManager.commit {
+                    setReorderingAllowed(true)  // Otimiza a transação
+                    replace(R.id.fragment_menu_container_admin, MenuFragment())
+                    addToBackStack(null)
                 }
             }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Libera o binding quando a view é destruída
     }
 }
