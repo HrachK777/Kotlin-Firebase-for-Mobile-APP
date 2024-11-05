2a7d58a7334ab484a914178c65562a4a914a216a
PedroAmaralFerreira <ferreira.pedromiguel@gmail.com>
2024-11-05 17:39:24 +0000

    Atualizado a apresentação da hora do feedback

--- app/src/main/java/ly/roast/roastly/data/model/Review.kt
+++ app/src/main/java/ly/roast/roastly/data/model/Review.kt
@@ -56,15 +56,37 @@ class FeedAdapter(
     }
 
     fun simplifyTimestamp(originalTimestamp: String): String {
-        val originalFormat = SimpleDateFormat("MMMM d, yyyy 'at' h:mm:ss a z", Locale.ENGLISH)
-        val targetFormat = SimpleDateFormat("MMM d, yyyy - h:mm a", Locale.ENGLISH)
+        val originalFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
+        //val targetFormat = SimpleDateFormat("MMM dd - HH:mm", Locale.ENGLISH)
 
         return try {
+            val reviewDate = originalFormat.parse(originalTimestamp)
+            val currentTime = System.currentTimeMillis()
+            val timeDifference = currentTime - reviewDate.time
+
+            when {
+                timeDifference < 60_000 -> "Agora mesmo"
+                timeDifference < 2 * 60_000 -> "Há 1 minuto"
+                timeDifference < 60 * 60_000 -> "Há ${timeDifference / 60_000} minutos"
+                timeDifference < 2 * 60 * 60_000 -> "Há uma hora"
+                timeDifference < 24 * 60 * 60_000 -> "Há ${timeDifference / (60 * 60_000)} horas"
+                timeDifference < 2 * 24 * 60 * 60_000 -> "Ontem"
+                timeDifference < 7 * 24 * 60 * 60_000 -> "Há ${timeDifference / (24 * 60 * 60_000)} dias"
+                timeDifference < 30 * 24 * 60 * 60_000 -> "Há ${timeDifference / (7 * 24 * 60 * 60_000)} semanas"
+                timeDifference < 365 * 24 * 60 * 60_000 -> "Há ${timeDifference / (30 * 24 * 60 * 60_000)} meses"
+                timeDifference < 2 * 365 * 24 * 60 * 60_000 -> "Há 1 ano"
+                else -> "Há ${timeDifference / (365 * 24 * 60 * 60_000)} anos"
+            }
+        } catch (e: Exception) {
+            originalTimestamp
+        }
+
+        /*return try {
             val date = originalFormat.parse(originalTimestamp)
             targetFormat.format(date)
         } catch (e: Exception) {
             originalTimestamp
-        }
+        }*/
     }
 
     override fun getItemCount() = reviews.size
