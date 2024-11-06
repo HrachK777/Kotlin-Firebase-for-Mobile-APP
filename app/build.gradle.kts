5e80a6d9ded9dd85e64e2f9dff4d52f0a725d0db
danny-gomes <dannnygomes@outlook.pt>
2024-11-06 10:41:55 +0000

    Alterado ratings dos rankings para nao serem editaveis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation ("com.squareup.picasso:picasso:2.8")
     implementation ("com.google.firebase:firebase-storage-ktx:20.2.0")
+    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("com.google.firebase:firebase-storage:20.2.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
